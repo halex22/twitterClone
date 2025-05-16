f60e8ce99b311596c7da12ebf7e5585fdb7425d4
halex22 <halex2209@gmail.com>
2025-05-16 09:05:11 +0200

    style sign up form

--- src/app/components/auth/sign-up/sign-up.component.ts
+++ src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,4 +10,9 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  router = inject(Router)
+
+  onClose(): void {
+    this.router.navigate(['/'])
+  }
 }
