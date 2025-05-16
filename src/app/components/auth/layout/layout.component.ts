b1f5ed8807d2c1ebbaf0125b7f418d18f2411b5a
halex22 <halex2209@gmail.com>
2025-05-16 12:17:26 +0200

    commit b4 refactor in and up components to implement the layout

---
+++ src/app/components/auth/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [],
+  template: `<ng-content />`,
+  // templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
--- src/app/components/auth/sign-in/sign-in.component.ts
+++ src/app/components/auth/sign-in/sign-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [LayoutComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
